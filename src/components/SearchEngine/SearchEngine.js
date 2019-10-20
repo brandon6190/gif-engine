78dc1f8bf2db14bbed256f5fcec5aeb6d87ab15d
brandon6190 <brandon6190@outlook.com>
2019-10-20 15:56:15 -0400

    -Importing makeStyles to manage component styling and class naming -Importing TextField from materual-ui for user input -Created variabled useStyles that is assigned the evaluation of makeStyles, which is an object with css rules Within SeachEngine:   -Created variable classes that will be an object with all of the classes and css rules   -Rendering a form element with TextField for input

--- src/components/SearchEngine/SearchEngine.js
+++ src/components/SearchEngine/SearchEngine.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+	searchEngine: {
+		border: '1px solid black',
+	},
+}));
 
 function SearchEngine() {
-  
-  return (
-    <div>
+	const classes = useStyles();
 
-    </div>
+	return (
+		<div className={classes.searchEngine}>
+			<form>
+				<TextField label="Search GIFs" type="search" />
+			</form>
+		</div>
 	);
 }
 
