6d1896ae9c640983917d34c4268b6a18c58d4322
brandon6190 <brandon6190@outlook.com>
2019-10-20 16:34:26 -0400

    -Importing Paper to use as a background for the heading and form field -Importing Typography to use styled typeface Within SearchEngine:   -Wrapping the heading and form with Paper   -Created h1 Heading for app title -Added root class to styles, and passed it to Paper

--- src/components/SearchEngine/SearchEngine.js
+++ src/components/SearchEngine/SearchEngine.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    margin: '50px',
+    padding: theme.spacing(3,2)
+  },
 	searchEngine: {
 		border: '1px solid black',
 	},
@@ -13,9 +19,14 @@ function SearchEngine() {
 
 	return (
 		<div className={classes.searchEngine}>
-			<form>
-				<TextField label="Search GIFs" type="search" />
-			</form>
+      <Paper className={classes.root}>
+        <Typography variant='h1' component='h1'>
+          GIFY Search Engine
+        </Typography>
+        <form>
+          <TextField label="Search GIFs" type="search" />
+        </form>
+      </Paper>
 		</div>
 	);
 }
