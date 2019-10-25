a9bd3c8d2a25d8360b913270727c7ad4a7816940
brandon6190 <brandon6190@outlook.com>
2019-10-24 21:09:19 -0400

    -Importing useContext and SearchEngineContext -Pulling appState with the updater function by calling useContext and passing SearchEngineContext -Created handleInputChange to update the state whatever the use is typing -Added handleInputChange to onChange Listener

--- src/components/SearchEngine/SearchField/UserInput.js
+++ src/components/SearchEngine/SearchField/UserInput.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SearchEngineContext } from '../../../Store';
 
 import TextField from '@material-ui/core/TextField';
 
 function UserInput() {
+  const [appState, updateState] = useContext(SearchEngineContext);
+
+  const handleInputChange = event => {
+    return updateState({type: 'INPUT_CHANGE', payload: event.target.value});
+  }
+
   return (
-    <TextField label="Search GIFs" type="search" />
+    <TextField onChange={handleInputChange} value={appState.searchTerm} label="Search GIFs" type="search" />
   );
 }
 
