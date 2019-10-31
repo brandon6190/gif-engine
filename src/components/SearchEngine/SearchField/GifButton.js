70eb185c34b7be243e12c3c524117ad6db0cf79d
brandon6190 <brandon6190@outlook.com>
2019-10-30 20:49:22 -0400

    handleUserClick: whenever the user clicks the button it send send a fetch request to the giphy api /search endpoint, and save the data to the appState

--- src/components/SearchEngine/SearchField/GifButton.js
+++ src/components/SearchEngine/SearchField/GifButton.js
@@ -4,6 +4,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import Button from '@material-ui/core/Button';
 
+const API_KEY = 'ano8y2sspOi6Udt2zE7MmAipRAr9NgPf';
+
 const useStyles = makeStyles(theme => ({
 	button: {
 		margin: theme.spacing(1),
@@ -11,15 +13,31 @@ const useStyles = makeStyles(theme => ({
 }));
 
 function GifButton() {
-  const { button } = useStyles();
-  const [appState] = useContext(SearchEngineContext);
-  
-  const handleClick = event => {
-    console.log('appState = ', appState.searchTerm);
-  }
+	const { button } = useStyles();
+	const [appState, updateState] = useContext(SearchEngineContext);
+
+	const handleUserClick = event => {
+		fetch(
+			`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${appState.searchTerm}&limit=20&offset=0&rating=G&lang=en`,
+    )
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log('data = ', data.data);
+      updateState({type: 'GIF_REQUEST', payload: data.data});
+    })
+    .catch(err => {
+      throw new Error('Error from api request');
+    })
+	};
 
 	return (
-		<Button className={button} onClick={handleClick} variant="contained" color="primary">
+		<Button
+			className={button}
+			onClick={handleUserClick}
+			variant="contained"
+			color="primary">
 			GIFs
 		</Button>
 	);
