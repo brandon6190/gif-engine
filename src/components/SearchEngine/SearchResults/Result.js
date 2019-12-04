5394f61714dd70b3abe200490e173a86d0a7a248
brandon6190 <brandon6190@outlook.com>
2019-12-03 21:38:54 -0500

    Added styling to Results component to add more padding and to change the opacity on hover

--- src/components/SearchEngine/SearchResults/Result.js
+++ src/components/SearchEngine/SearchResults/Result.js
@@ -2,26 +2,32 @@ import React from 'react';
 
 import Card from '@material-ui/core/Card';
 
-// import {makeStyles} from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 
-// const useStyles = makeStyles(theme => ({
-//   card: {
-//     widht: 
-//   }
-// }))
+const useStyles = makeStyles(theme => ({
+  singleImg: {
+    padding: '0 20px 20px',
+    '&:hover': {
+      opacity: '0.5',
+      transition: '0.5s'
+    }
+  }
+}));
 
 
 function Result(props) {
   console.log('gif = ', props.gif);
+  const {singleImg} = useStyles();
   const { url, slug, width, height} = props.gif;
   
   const cardStyles = {
     width: `${width}px`,
-    height: `${height}px`
+    height: `${height}px`,
+    display: 'inline'
   }
   
   return (
-    <Card style={cardStyles} >
+    <Card style={cardStyles} class={singleImg}>
       <img src={url} alt={slug} />
     </Card>
   )
