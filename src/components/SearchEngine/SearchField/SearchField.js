6d1172cfdc52ae46acf95ae2fe6263765a323064
brandon6190 <brandon6190@outlook.com>
2019-12-03 21:34:52 -0500

    Added component styling to increas to bottom margin of the SearchField container

--- src/components/SearchEngine/SearchField/SearchField.js
+++ src/components/SearchEngine/SearchField/SearchField.js
@@ -3,9 +3,19 @@ import React from 'react';
 import UserInput from './UserInput';
 import GifButton from './GifButton';
 
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  userInput: {
+    marginBottom: '15px'
+  }
+}));
+
 function SearchField() {
+  const {userInput} = useStyles();
+
   return (
-    <form>
+    <form class={userInput}>
       <UserInput />
       <GifButton />
     </form>
