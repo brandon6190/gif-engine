1a45e89744bc7429f32ef97796fc52014ce06ec7
brandon6190 <brandon6190@outlook.com>
2019-12-03 21:40:41 -0500

    Added styling to use flex properties for a more responsivegallery layout

--- src/components/SearchEngine/SearchResults/SearchResults.js
+++ src/components/SearchEngine/SearchResults/SearchResults.js
@@ -4,12 +4,24 @@ import { SearchEngineContext } from '../../../Store';
 
 import Result from './Result';
 
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  gallery: {
+    display: 'flex',
+    flexFlow: 'row wrap',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+}))
+
 function SearchResults() {
+  const { gallery } = useStyles();
   const [appState] = useContext(SearchEngineContext);
   
   
   return (
-    <div id='gallery'>
+    <div id='gallery' class={gallery}>
       {       
         appState.gifs.map(gif => {
           return <Result key={gif.id} id={gif.id} gif={gif.images.original} desc={gif.slug} />
